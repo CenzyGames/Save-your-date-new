9ffa88db7786888db3608edd83060d36651d5c97
Rajan Chawla <cenzy.games@gmail.com>
2016-07-18 00:40:49 +0530

    Menucontroller, Water effect, Scripts add

---
+++ Save your Date Unity/Assets/MenuController.cs
@@ -0,0 +1,121 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuController : MonoBehaviour {
+
+	public GameObject ExitMenu;
+	public GameObject SoundOn;
+	public GameObject SoundOff;
+	bool leaderShow;
+
+	// Use this for initialization
+	void Start () 
+	{
+		if(ExitMenu!=null)
+		{
+			ExitMenu = GameObject.Find("ExitMenu");
+			ExitMenu.SetActive(false);
+		}
+		if(SoundOn!=null)
+		{
+			SoundOn = GameObject.Find("SoundOn");
+			SoundOn.SetActive(true);
+		}
+		if(SoundOff!=null)
+		{
+			SoundOff = GameObject.Find("SoundOff");
+			SoundOff.SetActive(false);
+		}
+
+		leaderShow = false;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if(Input.GetKey(KeyCode.Escape))
+		{
+			BackHardwareKeyPress();
+		}
+	
+	}
+
+	void BackHardwareKeyPress()
+	{
+		
+	}
+	void SoundsOn()
+	{
+		AudioListener.volume = 1;
+		SoundOff.SetActive(false);
+		SoundOn.SetActive(true);
+	}
+	
+	void SoundsOff()
+	{
+		AudioListener.volume = 0;
+		SoundOn.SetActive(false);
+		SoundOff.SetActive(true);
+	}
+
+	public void PlayGame()
+	{
+		Application.LoadLevel("Gameplay");
+	}
+
+	public void SoundOnOff()
+	{
+		if(SoundOn.activeInHierarchy == true)
+		{
+			SoundsOff();
+		}
+		else
+		{
+			SoundsOn();
+		}
+	}
+
+	public void LeaderBoardController()
+	{
+		if(leaderShow == false)
+		{
+			LeaderboardShow();
+		}
+		else
+		{
+			LeaderboardHide();
+		}
+	}
+
+	void LeaderboardShow()
+	{
+
+	}
+
+	void LeaderboardHide()
+	{
+		
+	}
+
+	public void FacebookLogin()
+	{
+
+	}
+
+	public void ExitGame()
+	{
+		ExitMenu.SetActive(true);
+	}
+
+	public void ExitConfirmationYes()
+	{
+		Application.Quit();
+	}
+
+	public void ExitConfirmationNo()
+	{
+		if(ExitMenu!=null)
+		ExitMenu.SetActive(false);
+	}
+
+}
